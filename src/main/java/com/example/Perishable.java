883d00e202838529e3075c8402874a1008c476ea
Kathrin Trinh <trinhkathrin@hotmail.com>
2025-10-23 09:52:43 +0200

    Update Perishable.java

--- src/main/java/com/example/Perishable.java
+++ src/main/java/com/example/Perishable.java
@@ -3,5 +3,10 @@ package com.example;
 import java.time.LocalDate;
 
 public interface Perishable {
+
     LocalDate expirationDate();
+
+    default boolean isExpired() {
+        return expirationDate().isBefore(LocalDate.now());
+    }
 }
